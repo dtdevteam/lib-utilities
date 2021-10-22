19a476f344dbee32d951633a5ffb04f318c44d15
workjaedsada3 <work.jaedsada.ch@gmail.com>
2021-10-22 09:08:32 +0000

    add tracing id error

--- exception/exception.go
+++ exception/exception.go
@@ -1,8 +1,14 @@
-package exception
+package configs
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofrs/uuid"
+	// "github.com/labstack/gommon/log"
+)
 
 type ErrorHandle struct {
+	TracingId    string      `json:"tracing_id"`
 	ErrorStatus  int         `json:"error_status"`
 	ErrorMessage string      `json:"error_message"`
 	ErrorInput   interface{} `json:"error_input"`
@@ -11,8 +17,10 @@ type ErrorHandle struct {
 
 func ExceptionError(errorStatus int, err error) (int, ErrorHandle) {
 	var err_input interface{}
+	var tracing_id, _ = uuid.NewV4()
 
 	return errorStatus, ErrorHandle{
+		TracingId:    tracing_id.String(),
 		ErrorStatus:  errorStatus,
 		ErrorMessage: fmt.Sprintf("%s", err.Error()),
 		ErrorInput:   err_input,
@@ -20,7 +28,10 @@ func ExceptionError(errorStatus int, err error) (int, ErrorHandle) {
 }
 
 func ExceptionErrorValidation(errorStatus int, err error, err_input []map[string]interface{}) (int, ErrorHandle) {
+	var tracing_id, _ = uuid.NewV4()
+
 	return errorStatus, ErrorHandle{
+		TracingId:    tracing_id.String(),
 		ErrorStatus:  errorStatus,
 		ErrorMessage: fmt.Sprintf("%s", err.Error()),
 		ErrorInput:   err_input,
